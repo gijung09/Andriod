d35b3f1c4795b55e890f0bd7fc0394de520e248c
DESKTOP-991LD5M\user <qrt117@naver.com>
2021-05-26 19:31:53 +0900

    사용자 입력 및 출력 구현

--- 새 폴더/main.c
+++ 새 폴더/main.c
@@ -3,9 +3,12 @@
 int main()
 {
 	int age;
+	char name[10];
 	
 	printf("당신의 나이를 입력하세요 : ");
 	scanf("%d", &age);
-	
+	printf("당신의 이름을 입력하세요 : ", );
+	scanf("%s",&name);
+	printf("당신은 %s군요!\n", name);
 	printf("우와 당신은 %d살이군요!", age);
 }
